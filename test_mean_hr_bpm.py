b63f55dfe5a406581a25dec0cb835263c21e68e1
Roujia Wang <rosesharpaywang@RoujiamatoMacBook-Pro.local>
2018-10-26 19:39:52 -0400

    Debug the test

--- test_mean_hr_bpm.py
+++ test_mean_hr_bpm.py
@@ -1,19 +1,3 @@
-import pytest
-from mean_hr_bpm import mean_hr_bpm
-
-
 """
-@pytest.mark.parametrize("input, output", [
-    (test, NameError),
-    ('test', FileNotFoundError),
-    (1, ValueError),
-])
+place holder
 """
-@pytest.mark.parametrize("input1, input2, output", [
-    (60, 36, 36),
-    (50, 40, 75),
-    (42.5, 55.5, 60),
-])
-def test_mean_hr_bpm(input1, input2, output):
-    response = mean_hr_bpm(input1, input2)
-    assert response == output
