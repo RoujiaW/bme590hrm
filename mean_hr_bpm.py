340c12ce1934cba90e16165630f622691aa8c3b6
Roujia Wang <rosesharpaywang@RoujiamatoMacBook-Pro.local>
2018-10-27 12:17:08 -0400

    Remove duplicate files and group them into subfolder

--- mean_hr_bpm.py
+++
@@ -1,9 +0,0 @@
-def mean_hr_bpm(duration, num_beats, inputTime):
-    try:
-        inputTime = float(inputTime)
-        if inputTime <= 0 or inputTime >= 1e+6:
-            print("Time out of range. Assume 1 minute for heart rate")
-            inputTime = 1
-        return int(num_beats/duration * 60 * inputTime)
-    except ValueError:
-        print("Please enter a number with any punctuations such as 2")
--- userInput.py
+++
@@ -1,3 +0,0 @@
-def userInput():
-    inputTime = input("Time range in minutes (>0) for heart rate: ")
-    return inputTime
