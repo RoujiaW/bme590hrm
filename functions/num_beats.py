3b958920805304ebed36c061e6e0684065449499
RoujiaW <42785242+RoujiaW@users.noreply.github.com>
2018-10-27 13:02:42 -0400

    Update and include logging

--- functions/num_beats.py
+++ functions/num_beats.py
@@ -1,5 +1,22 @@
 def num_beats(rawList):
+    """
+    find the number of beats in whole ECG signal
+    :param filename: a list
+    :raises ImportError:  if the logging module not found
+    :returns: total number of entries in the list
+    :rtype: int
+    """
+    try:
+        import logging
+    except ImportError:
+        print("Could not import logging module")
+        return
+    else:
+        logging.basicConfig(filename='example.log', level=logging.DEBUG,
+                            filemode='w')
     try:
         return len(rawList)
+        logging.info("Run num_beats")
     except TypeError:
         print("Wrong type of input")
+        logging.error("Type error for num_beats")
