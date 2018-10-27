3e1ac6dea0e9e0206f6dc2da0fdacd96a56e89c1
Roujia Wang <rosesharpaywang@RoujiamatoMacBook-Pro.local>
2018-10-26 20:09:27 -0400

    Update hr_bpm function and test

--- test_mean_hr_bpm.py
+++ test_mean_hr_bpm.py
@@ -1,3 +1,13 @@
-"""
-place holder
-"""
+import pytest
+from mean_hr_bpm import mean_hr_bpm
+
+
+@pytest.mark.parametrize("input1, input2, input3, output", [
+    (20, 12, 2, 72),
+    (10, 3, 1.5, 27),
+    (60, 52, 100000000, 52),
+    (40, 32, "test", None),
+])
+def mean_hr_bpm(input1, input2, input3, output):
+    response = mean_hr_bpm(input1, input2, input3)
+    assert response == output
